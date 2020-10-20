52b1eda0644bff470483d024eacfab64776b6c1b
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-10-20 17:43:06 -0400

    Add files via upload

--- example.py
+++ example.py
@@ -18,6 +18,10 @@ movies = [
 
 print(movies[4][1][3])
 
+"""
+  Multiple-line comment
+"""
+
 def print_lol (the_list):
     for each_item in the_list:
         if isinstance(each_item, list):
@@ -27,5 +31,33 @@ def print_lol (the_list):
 
 print_lol(movies)
 
+is_hot = True
+is_cold = False
+if is_hot:
+   print("It's a hot day")
+   print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm cloth")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
 
 
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+   down_payment = price*0.1
+else:
+   down_payment = price*0.2
+print(f"Down payment: ${down_payment}")
+
+has_high_income = True
+has_good_credit = True
+has_criminal_record = False
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
